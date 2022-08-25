e96f4245c9fd4f9663b63f68dc9249c5ebb7938e
Divecho <github@divecho.com>
2022-08-25 17:24:58 +0530

    working demo

--- firebase.js
+++ firebase.js
@@ -7,7 +7,13 @@ import {
 import { getStorage } from "firebase/storage";
 import { initializeFirestore } from "firebase/firestore";
 const firebaseConfig = {
- // your config goes here
+  apiKey: "AIzaSyA5UXPbTuZ7MazZ2ShLWfwGwc7YuXVXCAE",
+  authDomain: "wp-clone-16076.firebaseapp.com",
+  projectId: "wp-clone-16076",
+  storageBucket: "wp-clone-16076.appspot.com",
+  messagingSenderId: "151892634397",
+  appId: "1:151892634397:web:e9c752e22f32ea63dc4ff4",
+  measurementId: "G-S4MDBLM3NE"
 };
 
 export const app = initializeApp(firebaseConfig);
